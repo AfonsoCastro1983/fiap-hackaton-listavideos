700022423c8c7d55267bfd1fa218b3398acc137b
AfonsoCastro1983 <afonso.castro@gmail.com>
2025-01-25 08:57:20 -0400

    Listagem de videos do usuário

---
+++ lambda_function.py
@@ -0,0 +1,36 @@
+import boto3
+
+dynamodb_client = boto3.resource('dynamodb')
+
+def lambda_handler(event, context):
+    # Obter o token JWT do header Authorization
+    token = event['headers']['Authorization'].split(" ")[1]
+
+    # Decodificar o token JWT
+    try:
+        decoded_token = jwt.decode(
+            token, 
+            algorithms=["RS256"], 
+            options={"verify_signature": False}
+        )
+        user_id = decoded_token['sub']
+    except Exception as e:
+        return {
+            "statusCode": 401,
+            "body": f"Token inválido: {str(e)}"
+        }
+    
+    table = dynamodb_client.Table("VideosTable")
+    
+    # Query para buscar vídeos do usuário
+    response = table.query(
+        KeyConditionExpression="userId = :userId",
+        ExpressionAttributeValues={
+            ":userId": user_id
+        }
+    )
+
+    return {
+        "statusCode": 200,
+        "body": response['Items']
+    }
